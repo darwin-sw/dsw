2535cd0005f6928d2e8cea084e73bd7af483bc43
campbell.webb <campbell.webb@36e70990-e246-58e2-6c0c-af78d3f76a8b>
2010-12-02 10:48:21 +0000

    multiple

---
+++ bin/instance2jpeg.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Conversion of OWL file to simple network diagram
+# Cam Webb <cwebb@oeb.harvard.edu>
+
+TMP1=`mktemp`.n3
+TMP2=`mktemp`.rdf
+TMP3=`mktemp`.dot
+TMP4=`mktemp`.dot
+
+# get the things that have ranges
+roqet -D ../dsw.owl --exec "
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+CONSTRUCT { ?s rdfs:range ?o}
+WHERE { ?s rdfs:range ?o }" > $TMP1
+
+# get the things that have domains
+roqet -D ../dsw.owl --exec "
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+CONSTRUCT { ?s rdfs:domain ?o}
+WHERE { ?s rdfs:domain ?o }" >> $TMP1
+
+# match them up
+roqet -D $TMP1 -r rdfxml --exec "
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX dwc: <http://rs.tdwg.org/dwc/terms/>
+PREFIX dsw: <http://darwin-sw.googlecode.com/svn/trunk/dsw.owl#>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+CONSTRUCT { ?s ?v ?o }
+WHERE { ?v rdfs:domain ?s .
+        ?v rdfs:range ?o . }" > $TMP2
+
+# convert to dot
+
+SIZECK=`mktemp`
+rapper -i rdfxml -o dot $TMP2 1> $TMP3 2> $SIZECK
+
+# check the size! (vital: dot will hog all resources and swapspace)
+
+SIZE=`grep "rapper: Parsing returned " $SIZECK | awk '{print \$4}'`
+
+if [ $SIZE -gt 200 ]
+then
+        echo "Stopping - too big for dot (" $SIZE " triples )" 
+        exit
+else
+
+    # rapper -i rdfxml -o dot $TMP3 > $TMP4
+    dot -T jpg $TMP3 > ../img/dsw.jpg
+
+fi
